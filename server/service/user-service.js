b0a3ebb6a545e5b01ff6444ac7e7b9cfe2b2cb65
KhOlAn <olegkhristenko@gmail.com>
2022-07-31 16:19:12 +0300

    Made server

---
+++ server/service/user-service.js
@@ -0,0 +1,95 @@
+const UserModel = require('../models/user-model')
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const mailService = require('./mail-service');
+const tokenService = require('./token-service');
+const UserDto = require('../dtos/user-dto');
+const {Error} = require("mongoose");
+const ApiError = require('../exceptions/api-error');
+
+class UserService {
+    async registration(email, password) {
+        const candidate = await UserModel.findOne({email});
+        if (candidate) {
+            throw ApiError.BadRequest(`Users with email ${email} already exists`)
+        }
+
+        const hashPassword = await bcrypt.hash(password, 3);
+        const activationLink = uuid.v4();
+
+        const user = await UserModel.create({email, password: hashPassword, activationLink});
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
+
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: userDto
+        }
+    }
+
+    async activate(activationLink) {
+        const user = await UserModel.findOne({activationLink});
+        if (!user) {
+            throw ApiError.BadRequest('Your link is not correct...')
+        }
+
+        user.isActivated = true;
+        await user.save();
+    }
+
+    async login(email, password) {
+        const user = await UserModel.findOne({email});
+        if (!user) {
+            throw ApiError.BadRequest("No user found with this email address...")
+        }
+
+        const isPassEquals = await bcrypt.compare(password, user.password);
+        if (!isPassEquals) {
+            throw ApiError.BadRequest("Invalid password...")
+        }
+
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: userDto
+        }
+    }
+
+    logout (refreshToken) {
+        return tokenService.removeToken(refreshToken);
+    }
+
+    async refresh (refreshToken) {
+        if (!refreshToken) {
+            throw ApiError.UnauthorizedError();
+        }
+
+        const userData = tokenService.validateRefreshToken(refreshToken);
+        const tokenFromDb = await tokenService.findToken(refreshToken);
+        if (!userData || !tokenFromDb) {
+            throw ApiError.UnauthorizedError();
+        }
+
+        const user = await UserModel.findById(userData.id);
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: userDto
+        }
+    }
+
+    getAllUsers() {
+        return UserModel.find();
+    }
+}
+
+module.exports = new UserService();
